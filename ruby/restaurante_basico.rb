3bff5084d98fe7456cb5ddfbfdc16415c17b727c
brunormussolin <44055856+brunormussolin@users.noreply.github.com>
2018-10-15 22:24:23 -0300

    Update restaurante_basico.rb

--- ruby/restaurante_basico.rb
+++ ruby/restaurante_basico.rb
@@ -1,3 +1,12 @@
-puts "Digite o nome do restaurante"
-nome = gets
-print "Nome do restaurante: #{nome}"
+def procura_sede_copa_do_mundo( ano )
+case ano
+when 1895..2005
+"Não lembro... :)"
+when 2006
+"Alemanha"
+when 2010
+"África do Sul"
+when 2014
+"Brasil"
+end
+end
