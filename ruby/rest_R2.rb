302b7b48ec356fcd4b14c86fe7f858c6962b3a85
brunormussolin <44055856+brunormussolin@users.noreply.github.com>
2018-10-22 10:51:02 -0300

    Update rest_R2.rb

--- ruby/rest_R2.rb
+++ ruby/rest_R2.rb
@@ -1,5 +1,17 @@
-def qualifica(nota, msg="Obrigado")
-puts "A nota do restaurante foi #{nota}. #{msg}"
+class Restaurante
+  attr_accessor :nota
+  def initialize(nome)
+    puts "criando um novo restaurante: #{nome}"
+    @nome = nome
+  end
+
+  def qualifica(msg="Obrigado")
+    puts "A nota do #{@nome} foi #{@nota}. #{msg}"
+  end
 end
-qualifica(10)
-qualifica(1, "Comida ruim.")
+restaurante_um = Restaurante.new("Fasano")
+restaurante_dois = Restaurante.new("Fogo de Chao")
+restaurante_um.nota = 10
+restaurante_dois.nota = 1
+restaurante_um.qualifica
+restaurante_dois.qualifica("Comida ruim.")
