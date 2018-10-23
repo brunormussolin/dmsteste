aeb59075b034cb2f472098d4e91931172c33e49e
brunormussolin <44055856+brunormussolin@users.noreply.github.com>
2018-10-23 09:19:34 -0300

    Update 20181023120736_create_restaurantes.rb

--- vota_prato/db/migrate/20181023120736_create_restaurantes.rb
+++ vota_prato/db/migrate/20181023120736_create_restaurantes.rb
@@ -1,7 +1,8 @@
 class CreateRestaurantes < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurantes do |t|
-
+      t.string :nome, limit: 80
+      t.string :endereco
       t.timestamps
     end
   end
