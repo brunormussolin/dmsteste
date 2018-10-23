e3529191ba2895c33b544e23cec60d3f8c3782a2
brunormussolin <44055856+brunormussolin@users.noreply.github.com>
2018-10-23 14:52:55 -0300

    Update routes.rb

--- vota_prato/config/routes.rb
+++ vota_prato/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  match 'ola' => 'ola_mundo#index', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
