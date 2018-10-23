7ab541dd25235012417e9238ee52a3529c215ea3
brunormussolin <44055856+brunormussolin@users.noreply.github.com>
2018-10-23 14:22:13 -0300

    Update restaurante.rb

--- vota_prato/app/models/restaurante.rb
+++ vota_prato/app/models/restaurante.rb
@@ -8,4 +8,6 @@ class Restaurante < ApplicationRecord
   private def primeira_letra_deve_ser_maiuscula
     errors.add(:nome, "primeira letra deve ser maiúscula") unless nome =~ /[A-Z].*/
   end
+  has_many :qualificacoes
+  has_and_belongs_to_many :pratos 
 end
