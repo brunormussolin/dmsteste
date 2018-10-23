d33e18585b3cf90101f4c5bd1255a22579de32be
brunormussolin <44055856+brunormussolin@users.noreply.github.com>
2018-10-23 13:30:24 -0300

    Update 20181023161512_create_clientes.rb

--- vota_prato/db/migrate/20181023161512_create_clientes.rb
+++ vota_prato/db/migrate/20181023161512_create_clientes.rb
@@ -1,7 +1,8 @@
 class CreateClientes < ActiveRecord::Migration[5.2]
   def change
     create_table :clientes do |t|
-
+      t.string :nome, limit: 80
+      t.integer :idade 
       t.timestamps
     end
   end
