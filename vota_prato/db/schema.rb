f84c4110daaf1415b28639be8f0222df841b71b1
brunormussolin <bruno-fei@hotmail.com>
2018-10-23 16:53:06 +0000

    rev5

--- vota_prato/db/schema.rb
+++ vota_prato/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_161512) do
+ActiveRecord::Schema.define(version: 2018_10_23_164700) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "nome", limit: 80
     t.integer "idade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pratos", force: :cascade do |t|
+    t.string "nome", limit: 80
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
